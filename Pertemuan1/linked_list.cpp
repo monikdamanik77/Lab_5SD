57659930de686e25a99edbdd20a20e5a82ff43e3
monikdamanik77 <monikadamanik2007@gmail.com>
2026-09-11 10:02:00 +0700

    Add files via upload

---
+++ Pertemuan1/linked_list.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+struct stud {  
+              int   roll;
+              char  name[25];
+              int   age;
+              struct stud *next;
+            };
+
+   /* A user-defined data type called “node” */
+
+typedef struct stud node;
+node *head;
+
+node *create_list() 
+{ 
+    int  k, n; 
+    node  *p, *head; 
+
+    printf  ("\n How many elements to enter?"); 
+     scanf ("%d", &n); 
+
+    for  (k=0; k<n; k++) 
+    { 
+        if (k == 0) {
+          head = (node *) malloc(sizeof(node)); 
+          p = head; 
+	     }
+	     else {
+               p->next  = (node *) malloc(sizeof(node)); 
+               p = p->next;      
+	          }
+	  
+        scanf ("%d %s %d", &p->roll, p->name, &p->age); 
+    } 
+
+    p->next  =  NULL; 
+    return (head);
+} 
+
+void display (node *head)
+{
+  int  count = 1;
+  node  *p;
+    
+  p = head;
+  while (p != NULL)
+  {
+    printf ("\nNode %d: %d %s %d", count, 
+                   p->roll, p->name, p->age);
+    count++;
+    p = p->next;      
+  }
+  printf ("\n");
+}
+
+void insert (node **head) 
+{ 
+    int  k = 0, rno; 
+    node *p, *q, *new; 
+    
+    new = (node *) malloc(sizeof(node)); 
+
+    printf ("\nData to be inserted: ");
+      scanf ("%d %s %d", &new->roll, new->name, &new->age); 
+    printf ("\nInsert before roll (-ve for end):"); 
+      scanf ("%d", &rno); 
+
+    p = *head; 
+
+    if (p->roll == rno)      /* At the beginning */ 
+    { 
+        new->next = p; 
+        *head = new; 
+    } 
+
+        else 
+    {
+	while ((p != NULL) && (p->roll != rno))      
+        { 
+            q = p; 
+            p = p->next; 
+        }      
+    
+        if  (p == NULL)       /* At the end */ 
+        { 
+            q->next = new;    
+            new->next = NULL;     
+        } 
+	 else if  (p->roll  == rno)     
+                           /* In the middle */ 
+                { 
+                    q->next = new; 
+                    new->next = p; 
+                } 
+    }
+} 
+
+void  delete (node **head) 
+{ 
+    int  rno; 
+    node  *p, *q; 
+    
+    printf ("\nDelete for roll :"); 
+      scanf ("%d", &rno); 
+
+    p = *head; 
+    if  (p->roll == rno)            
+             /* Delete the first element */ 
+    { 
+        *head = p->next;   
+        free (p); 
+    } 
+
+       else
+    {
+        while  ((p != NULL) && (p->roll != rno))      
+        { 
+            q = p; 
+            p  =  p->next; 
+        }      
+
+        if  (p == NULL)      /* Element not found */ 
+           printf ("\nNo match :: deletion failed");
+       
+        else if (p->roll == rno)           
+                      /* Delete any other element */ 
+             {        
+                 q->next  =  p->next; 
+                 free (p); 
+             } 
+
+             
---
+++ Pertemuan1/melatih_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    char string1[20], string2[] = "string literal";
+    cout << "enter a string";
+    cin>> string1;
+    cout<< "string1 is: "<< string1
+        << "\nstring2 is: "<< string2
+        << "\nstring1 with space between characters is:\n";
+
+for (int i=0; stringl[1] != '\0'; i++) {
+
+cout << stringl [1] <<'';
+
+cin >> string); // reads "there"
+
+cout << "\nstringl is: " << stringl << endl;
+
+return 0;
